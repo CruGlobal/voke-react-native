6a901fb5e45aa70979772799a7cf801dcf5b444b
Bryan Eaton <bryaneaton13@gmail.com>
2017-10-19 10:37:44 -0400

    Implement a search state while searching for contacts and its taking a long time.

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -151,12 +151,16 @@ class ContactsList extends Component {
   }
 
   render() {
-    const { items } = this.props;
+    const { items, isSearching } = this.props;
     // Don't do a length check, it can be taxing on large arrays
     if (!items[0]) {
       return (
         <Flex align="center" justify="center">
-          <Text style={styles.noResultsText}>No results to display</Text>
+          <Text style={styles.noResultsText}>
+            {
+              isSearching ? 'Finding contacts...' : 'No results to display'
+            }
+          </Text>
         </Flex>
       );
     }
