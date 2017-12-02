6b8b5bb2c4fca86dad0bfdca1e733fbf87740cf8
Ben Gauthier <bengauthier@Bens-MacBook-Pro.local>
2017-12-02 09:43:22 -0500

    remove ios rnn configs

--- ios/Voke/AppDelegate.m
+++ ios/Voke/AppDelegate.m
@@ -9,14 +9,6 @@
 
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 
-// **********************************************
-// *** DON'T MISS: THE NEXT LINE IS IMPORTANT ***
-// **********************************************
-#import "RCCManager.h"
-
-// IMPORTANT: if you're getting an Xcode error that RCCManager.h isn't found, you've probably ran "npm install"
-// with npm ver 2. You'll need to "npm install" with npm 3 (see https://github.com/wix/react-native-navigation/issues/1)
-
 #import <React/RCTRootView.h>
 
 @implementation AppDelegate
