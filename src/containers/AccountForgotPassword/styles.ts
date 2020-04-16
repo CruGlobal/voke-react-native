64335f22a28df85039183e99124896fe3dfa4a4a
ArielWolffe <leighwolffedigital@gmail.com>
2020-04-16 17:32:10 -0400

    VO-807-Fixed font size and keyboard view

--- src/containers/AccountForgotPassword/styles.ts
+++ src/containers/AccountForgotPassword/styles.ts
@@ -1,55 +1,72 @@
-import {ReactText} from 'react';
+import { ReactText } from 'react';
 import { css, ReactNativeStyle } from '@emotion/native';
 import theme from '../../theme';
 import ui from '../../ui';
 
 const styles: { [key: string]: any } = {
-	...ui,
-	SectionOnboarding: css`
-		background-color: ${theme.colors.blue};
-	`,
-	SectionAction: css`
-		background-color: ${theme.colors.secondary};
-		padding-left:${theme.spacing.xl + 'px'};
-		padding-right:${theme.spacing.xl + 'px'};
-	`,
-	ButtonStart: [
-		ui.button.size.l,
-		ui.button.style.solid,
-		css`
-			margin-left:${theme.spacing.l + 'px'};
-			margin-right:${theme.spacing.l + 'px'};
-		`,
-	],
-	ButtonStartLabel: [
-		ui.buttonText.size.l
-	],
-	ButtonSignIn: [
-		ui.button.size.m,
-		ui.button.style.outline
-	],
-	ButtonSignInLabel: [
-		ui.buttonText.size.m
-	],
-	SignInText: css`
-		color: ${theme.colors.white};
-		font-size: ${theme.fontSizes.l + 'px'};
-		font-family: ${theme.fonts.regular};
-	`,
-	TextSmall: css`
-		color: ${theme.colors.white};
-		font-size:12px;
-		font-family: ${theme.fonts.regular};
-	`,
-	Link: css`
-		text-decoration-line: underline;
-		color: ${theme.colors.white};
-	`,
-	SectionSignIn: css`
-		background-color: ${theme.colors.secondary};
-		padding-bottom:${theme.spacing.xl + 'px'};
-	`,
-}
+  ...ui,
+  MainContainer: [
+    ui.container.default,
+    css`
+      flex: 1;
+      background-color: ${theme.colors.secondary};
+    `,
+  ],
+  PrimaryContent: css`
+    width: 100%;
+    background-color: ${theme.colors.primary};
+    padding-left: ${`${theme.spacing.xl}px`};
+    padding-right: ${`${theme.spacing.xl}px`};
+  `,
+  Divider: css`
+    width: 100%;
+    background-color: ${theme.colors.primary};
+  `,
+  SectionAction: css`
+    width: 100%;
+    background-color: ${theme.colors.secondary};
+    padding-left: ${`${theme.spacing.xl}px`};
+    padding-right: ${`${theme.spacing.xl}px`};
+    padding-bottom: ${`${theme.spacing.l}px`};
+  `,
+  ButtonStart: [
+    ui.button.size.l,
+    ui.button.style.solid,
+    css`
+      margin-top: ${`${theme.spacing.m}px`};
+      margin-left: ${`${theme.spacing.l}px`};
+      margin-right: ${`${theme.spacing.l}px`};
+      margin-bottom: ${`${theme.spacing.l}px`};
+    `,
+  ],
+  ButtonStartLabel: [ui.buttonText.size.l],
+  ButtonSignIn: [ui.button.size.m, ui.button.style.outline],
+  ButtonSignInLabel: [ui.buttonText.size.m],
+  SignInText: css`
+    color: ${theme.colors.white};
+    font-size: ${`${theme.fontSizes.l}px`};
+    font-family: ${theme.fonts.regular};
+  `,
+  TextSmall: css`
+    color: ${theme.colors.white};
+    font-size: 12px;
+    font-family: ${theme.fonts.regular};
+  `,
+  TextLarge: css`
+    color: ${theme.colors.white};
+    font-size: ${`${theme.fontSizes.l}px`};
+    font-family: ${theme.fonts.regular};
+    padding-right: ${`${theme.spacing.m}px`};
+    padding-left: ${`${theme.spacing.m}px`};
+  `,
+  Link: css`
+    text-decoration-line: underline;
+    color: ${theme.colors.white};
+  `,
+  SectionSignIn: css`
+    background-color: ${theme.colors.secondary};
+    padding-bottom: ${`${theme.spacing.xl}px`};
+  `,
+};
 
-
-export default styles
+export default styles;
