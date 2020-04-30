f820368d2ae2e69d5c2d3c12932e5c7642c1a0cd
lumberman <vlad@lumbermandesigns.com>
2020-04-30 02:03:42 -0300

    VC-831 Adventure > Inner Video Chat > In-app Notifications not working closes #42

--- src/containers/AdventureActive/index.tsx
+++ src/containers/AdventureActive/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useSafeArea } from 'react-native-safe-area-context';
-import Flex from '../../components/Flex';
-import st from '../../st';
-import { ScrollView, Linking, FlatList } from 'react-native';
+import { ScrollView, FlatList } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import { getMyAdventure } from '../../actions/requests';
 import { RootState } from '../../reducers';
-import Video from '../../components/Video';
-import { useNavigation } from '@react-navigation/native';
 import { useMount } from '../../utils';
-import { getAdventureSteps } from '../../actions/requests';
 import AdventureStepCard from '../../components/AdventureStepCard';
+import Flex from '../../components/Flex';
 import Text from '../../components/Text';
+import Video from '../../components/Video';
+import st from '../../st';
 
 type AdventureActiveProps = {
   route: {
@@ -18,84 +17,105 @@ type AdventureActiveProps = {
       adventureId: string;
     };
   };
-}
+};
 
 function AdventureActive({ route }: AdventureActiveProps): React.ReactElement {
   const { adventureId } = route.params;
   const { myAdventures } = useSelector(({ data }: RootState) => data) || {};
-  const adventure =
-    myAdventures.find((item: { id: string }) => item.id === adventureId) || {};
+  const [adventure, setAdventure] = useState(
+    myAdventures.find((item: { id: string }) => item.id === adventureId) || {}
+  );
+
   const dispatch = useDispatch();
   const insets = useSafeArea();
   const [isPortrait, setIsPortrait] = useState(true);
-  const steps = useSelector(({ data }) => data.adventureSteps);
+  const steps = useSelector(({ data }: RootState) => data.adventureSteps);
   const [currentSteps, setCurrentSteps] = useState(steps[adventureId] || []);
+
+  const getPendingAdventure = async (): Promise<void> => {
+    const pendingAdventure = await dispatch(getMyAdventure(adventureId));
+    if (pendingAdventure && pendingAdventure?.id) {
+      setAdventure(pendingAdventure);
+    }
+  };
+
   useMount(() => {
-    dispatch(getAdventureSteps(adventureId));
+    // If adventure wasn't accepeted by a friend yet and not started.
+    // (if adventure.status = pending)
+    if (Object.keys(adventure).length === 0) {
+      getPendingAdventure();
+    }
+    // dispatch(getAdventureSteps(adventureId));
+    // -- ☝️call to update steps from the server. (NOT SURE IF REALLY NEEDED)
   });
+
   useEffect(() => {
     setCurrentSteps(steps[adventureId]);
-  }, [steps]);
+  }, [adventure, steps, currentSteps]);
 
   return (
     <Flex value={1}>
       <ScrollView bounces style={[st.bgBlue, { paddingBottom: insets.bottom }]}>
-        { adventure && <Video
-          onOrientationChange={(orientation: string): void => {
-            if (orientation === 'portrait') {
-              setIsPortrait(true);
-            } else {
-              setIsPortrait(false);
-            }
-          }}
-          item={adventure?.item?.content}
-        >
-          <Flex direction="column" align="center">
-            {/* Call to action overlay to be rendered over the video. */}
-            <Text
-              style={{
-                fontSize: 24,
-                paddingHorizontal: 25,
-                paddingVertical: 4,
-                color: 'white',
-              }}
-            >
-              {adventure.name}
-            </Text>
-            <Flex
-              style={{
-                borderRadius: 20,
-                backgroundColor: 'rgba(0,0,0,0.8)',
-                // minWidth: '50%',
-                alignSelf: 'center',
-                marginBottom: 10,
-              }}
-            >
+        {adventure && (
+          <Video
+            onOrientationChange={(orientation: string): void => {
+              if (orientation === 'portrait') {
+                setIsPortrait(true);
+              } else {
+                setIsPortrait(false);
+              }
+            }}
+            item={adventure?.item?.content}
+          >
+            <Flex direction="column" align="center">
+              {/* Call to action overlay to be rendered over the video. */}
               <Text
                 style={{
-                  fontSize: 16,
-                  paddingHorizontal: 24,
-                  paddingTop: 8,
-                  paddingBottom: 10,
-                  textAlign: 'center',
+                  fontSize: 24,
+                  paddingHorizontal: 25,
+                  paddingVertical: 4,
                   color: 'white',
                 }}
               >
-                Watch Trailer
+                {adventure.name}
               </Text>
+              <Flex
+                style={{
+                  borderRadius: 20,
+                  backgroundColor: 'rgba(0,0,0,0.8)',
+                  // minWidth: '50%',
+                  alignSelf: 'center',
+                  marginBottom: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 16,
+                    paddingHorizontal: 24,
+                    paddingTop: 8,
+                    paddingBottom: 10,
+                    textAlign: 'center',
+                    color: 'white',
+                  }}
+                >
+                  Watch Trailer
+                </Text>
+              </Flex>
             </Flex>
-          </Flex>
-        </Video> }
+          </Video>
+        )}
 
         {isPortrait && (
           <FlatList
-            renderItem={props => (
+            data={currentSteps}
+            renderItem={(step): React.ReactElement => (
               <AdventureStepCard
-                {...props}
-                adventure={adventure} //!!!
+                // {...props}
+                step={step.item}
+                steps={currentSteps}
+                adventure={adventure} //! !!
               />
             )}
-            data={currentSteps}
             style={[
               st.w(st.fullWidth),
               st.pt4,
