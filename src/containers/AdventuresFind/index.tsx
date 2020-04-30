ff8b8ebf24b4ab1d84e87e651d05e9ec4be2c1b5
lumberman <vlad@lumbermandesigns.com>
2020-04-30 02:21:29 -0300

    VC-827 Find Adventures > Content wont show until pull to update gesture #43

--- src/containers/AdventuresFind/index.tsx
+++ src/containers/AdventuresFind/index.tsx
@@ -1,55 +1,43 @@
-import React, { useState, useRef, forwardRef, useEffect } from 'react';
-import Orientation from 'react-native-orientation-locker';
-import { useSafeArea } from 'react-native-safe-area-context';
-import Flex from '../../components/Flex';
-import Text from '../../components/Text';
-import StatusBar from '../../components/StatusBar';
-import VokeIcon from '../../components/VokeIcon';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ScrollView, FlatList } from 'react-native';
 import { useMount } from '../../utils';
-
+import { RootState } from '../../reducers';
+import { getAvailableAdventures } from '../../actions/requests';
+import AvailableAdventureItem from '../../components/AvailableAdventureItem';
+import AdventuresActions from '../AdventuresActions';
 import st from '../../st';
 import theme from '../../theme';
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
-import { ActivityIndicator, ScrollView, FlatList } from 'react-native';
 
-import Touchable from '../../components/Touchable';
-import {
-  getAvailableAdventures,
-  getMyAdventures,
-  getAdventuresInvitations,
-} from '../../actions/requests';
-import AvailableAdventureItem from '../../components/AvailableAdventureItem';
-import Triangle from '../../components/Triangle';
-import AdventuresActions from '../AdventuresActions';
-
-
-const AdventuresFind = () => {
+const AdventuresFind = (): React.ReactElement => {
   const availableAdventures = useSelector(
-    ({ data }) => data.availableAdventures,
+    ({ data }: RootState) => data.availableAdventures
   );
   const [adventures, setAdventures] = useState(availableAdventures);
   const dispatch = useDispatch();
+
   useMount(() => {
     if (availableAdventures.length === 0) {
       dispatch(getAvailableAdventures());
     }
   });
+
   useEffect(() => {
     setAdventures(availableAdventures);
   }, [availableAdventures]);
+
   return (
     <ScrollView style={[st.f1, st.bgBlue]}>
       <AdventuresActions />
       <FlatList
-        renderItem={props => <AvailableAdventureItem {...props} />}
+        renderItem={(props): React.ReactElement => (
+          <AvailableAdventureItem {...props} />
+        )}
         data={adventures}
         style={[st.w(st.fullWidth)]}
-        removeClippedSubviews={true}
       />
     </ScrollView>
   );
-}
+};
 
 export default AdventuresFind;
