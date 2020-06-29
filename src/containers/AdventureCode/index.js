6a54028cfd986179de9abbdd8e70b7cb9bda6ab9
lumberman <vlad@lumbermandesigns.com>
2020-06-29 16:55:34 -0300

    Welcome screen invite " I have a code " not working

--- src/containers/AdventureCode/index.js
+++ src/containers/AdventureCode/index.js
@@ -1,30 +1,35 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSafeArea } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../reducers';
 import { useTranslation } from "react-i18next";
 import { KeyboardAvoidingView, Alert, Keyboard } from 'react-native';
 import Flex from '../../components/Flex';
 import Text from '../../components/Text';
 import NameInput from '../../components/NameInput';
-import Image from '../../components/Image';
 import StatusBar from '../../components/StatusBar';
 import BotTalking from '../../components/BotTalking'
-import VokeIcon from '../../components/VokeIcon';
 import st from '../../st';
 import Button from '../../components/Button';
 import { toastAction } from '../../actions/info';
 
 // import { MONTHLY_PRICE } from '../../constants';
 import theme from '../../theme';
-
-import Touchable from '../../components/Touchable';
+import { createAccount, updateMe } from '../../actions/auth';
 import { acceptAdventureInvitation } from '../../actions/requests';
 
 function AdventureCode(props) {
   const { t } = useTranslation('haveCode');
   const insets = useSafeArea();
   const navigation = useNavigation();
+  const isLoggedIn = useSelector(({ auth }: RootState) => auth.isLoggedIn);
+  const userId = useSelector(
+    ({ auth }: RootState) => auth.user.id,
+  );
+  const firstName = useSelector(
+    ({ auth }: RootState) => auth.user.firstName,
+  );
   const [adventureCode, setAdventureCode] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
@@ -54,24 +59,58 @@ function AdventureCode(props) {
     if (adventureCode.length > 3) {
       try {
         setIsLoading(true);
+        let currentUserId = null;
+
+        // Before proceeding with invite,
+        // check if current user logged-in?
+        if (!isLoggedIn) {
+          // Create New Account.
+          const results = await dispatch(
+            // Need '' otherwise API will show 'null' in the user profile later.
+            createAccount({
+              first_name: '',
+              last_name: '',
+            }),
+          );
+          currentUserId = results?.user?.id || null
+        } else {
+          currentUserId = userId;
+        }
+
+        // acceptAdventureInvitation will return 'Invalid Invite',
+        // if the users not set.
+        if (!currentUserId) {
+          setIsLoading(false);
+          return;
+        }
 
         const newAdventure = await dispatch(
           acceptAdventureInvitation(adventureCode),
         );
-        const isGroup = newAdventure.kind === 'multiple';
-        if (isGroup) {
-          navigation.navigate('GroupModal', {
-            adventure: newAdventure || {},
-          });
+
+        if (!firstName) {
+          // No first name > user came here from the Welcome screen,
+          // continue onboarding process.
+          navigation.navigate('AccountName')
         } else {
-          if ( newAdventure?.messenger_journey_id ) {
-            // GO STRAIGHT INTO ADVENTURE
-            navigation.navigate('AdventureActive', {
-              adventureId: newAdventure?.messenger_journey_id,
-            })
+          // Otherwise, invitation accepted from within an app.
+          const isGroup = newAdventure.kind === 'multiple';
+          // After entering invite code redirect the user
+          // to the group modal or Adventure steps screen.
+          if (isGroup) {
+            navigation.navigate('GroupModal', {
+              adventure: newAdventure || {},
+            });
           } else {
-            navigation.goBack();
+            if ( newAdventure?.messenger_journey_id ) {
+              // Go straight into Adventure.
+              navigation.navigate('AdventureActive', {
+                adventureId: newAdventure?.messenger_journey_id,
+              })
+            }
           }
+          // Backup plan: Redirect to My Adventures screen.
+          navigation.navigate('LoggedInApp');
         }
       } catch (error) {
         dispatch(toastAction( t('signUp:invalidCode'), 'short' ));
