ca99dfa575d7906066fc58a601904610bf75ce10
lumberman <vlad@mitkovsky.com>
2020-11-10 11:16:49 -0400

    Trim Adventure Code

--- src/containers/AdventureCode/index.tsx
+++ src/containers/AdventureCode/index.tsx
@@ -159,7 +159,7 @@ function AdventureCode(): ReactElement {
           label={t('adventureCode:adventureCode')}
           placeholder="000000"
           value={adventureCode}
-          onChangeText={text => setAdventureCode(text)}
+          onChangeText={text => setAdventureCode(text.trim())}
           returnKeyType="done"
           onSubmitEditing={handleContinue}
           keyboardType="number-pad"
