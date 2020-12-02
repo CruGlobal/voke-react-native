ba707fdf9b01892a2d0c1a5430bb4b296c524fd3
lumberman <vlad@mitkovsky.com>
2020-12-02 15:34:11 -0400

    Quick Tabs Styling Adjustment

--- src/components/CustomTabs/TabBarStyled.tsx
+++ src/components/CustomTabs/TabBarStyled.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { TabBar } from 'react-native-tab-view';
+import theme from 'utils/theme';
 
 import Text from '../Text';
-import theme from 'utils/theme';
 
 import styles from './styles';
 
@@ -17,7 +17,11 @@ const TabBarStyled = (props: any): React.ReactElement => {
       // inactiveColor={theme.colors.secondary}
       // Param renderLabel can have the next params: ({ route, focused, color }).
       renderLabel={({ route, focused }): React.ReactElement => (
-        <Text style={styles['tabBarTitle' + tabsTheme + (focused ? 'Active' : '')]}>{route.title}</Text>
+        <Text
+          style={styles['tabBarTitle' + tabsTheme + (focused ? 'Active' : '')]}
+        >
+          {route.title}
+        </Text>
       )}
       {...props}
     />
