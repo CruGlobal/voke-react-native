84aa65454565a96ed20bdfffe2f9fb105355ff7e
lumberman <vlad@mitkovsky.com>
2020-12-22 19:59:24 -0400

    WIP

--- ios/Voke/AppDelegate.h
+++ ios/Voke/AppDelegate.h
@@ -1,7 +1,11 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+// Voke: Notifications
+// https://github.com/react-native-community/push-notification-ios#update-appdelegateh
+#import <UserNotifications/UNUserNotificationCenter.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+// @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
--- ios/Voke/AppDelegate.m
+++ ios/Voke/AppDelegate.m
@@ -4,6 +4,15 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+// Voke: ===============================================================
+#import <Firebase.h> // Voke: https://rnfirebase.io/#validate-ios-credentials
+#import <FBSDKCoreKit/FBSDKCoreKit.h> // Voke: FB login
+#import "Orientation.h" // Voke: Needed to handle orientation changes
+#import <RNCPushNotificationIOS.h> // Voke: Needed for push notifications
+#import <UserNotifications/UserNotifications.h> // Voke: Needed for push notifications
+#import <React/RCTLinkingManager.h> //Voke: Deeplinking
+#import "RNBootSplash.h" //Voke: Launch screen.
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -47,6 +56,27 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // Voke: ===============================================================
+  // Voke: Launch screen.
+  // https://github.com/zoontek/react-native-bootsplash#ios-1
+  [RNBootSplash initWithStoryboard:@"LaunchScreen" rootView:rootView];
+
+  // Voke: Push notifications (Define UNUserNotificationCenter)
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+
+  // Voke: Configure Firebase
+  // https://rnfirebase.io/#validate-ios-credentials
+  // if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+    // [Fabric with:@[CrashlyticsKit]];
+  // }
+
+  // VOKE: Facebook Login SDK
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+    didFinishLaunchingWithOptions:launchOptions];
+
   return YES;
 }
 
@@ -57,6 +87,82 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+// Voke: ===============================================================
+// Voke: Facebook SDK
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  // Voke: Deeplink from Facebook
+  BOOL handleFBSDK = [[FBSDKApplicationDelegate sharedInstance] application:application
+    openURL:url
+    sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+  ];
+  // Voke: Deeplink for React Native
+  BOOL handleRCT = [RCTLinkingManager application:application
+    openURL:url
+    options:options
+  ];
+  // Voke: Deeplink for Firebase?
+  // BOOL handleFirebase = [[RNFBDynamicLinksAppDelegateInterceptor sharedInstance] application:application openURL:url options:options];
+  return handleFBSDK || handleRCT;
+}
+// Voke: Needed for handling orientation changes
+// https://github.com/wonday/react-native-orientation-locker
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+  return [Orientation getOrientation];
+}
+// Voke: Notifications
+// https://github.com/react-native-community/push-notification-ios#update-appdelegatem
+// Voke: Required to register for notifications
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+ [RNCPushNotificationIOS didRegisterUserNotificationSettings:notificationSettings];
+}
+// Voke: Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+ [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+// Voke: Required for the notification event.
+// You must call the completion handler after handling the remote notification!
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+// Voke: Required for the registrationError event.
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+ [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+}
+// Voke: IOS 10+ Required for localNotification event
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
+  completionHandler();
+}
+// Voke: IOS 4-10 Required for the localNotification event.
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+ [RNCPushNotificationIOS didReceiveLocalNotification:notification];
+}
+// Voke: Called when a notification is delivered to a foreground app.
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+}
+// Voke: React Native Universal Links.
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
 }
 
 @end
