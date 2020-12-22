ddf7c9039ea3b64cc250f4f9a9a130adf081932f
lumberman <vlad@mitkovsky.com>
2020-12-22 11:43:44 -0400

    WIP

--- ios/VokeTests/VokeTests.m
+++ ios/VokeTests/VokeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VokeRN64Tests : XCTestCase
+@interface VokeTests : XCTestCase
 
 @end
 
-@implementation VokeRN64Tests
+@implementation VokeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
