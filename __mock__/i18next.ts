2ae90d6f4971d37732eec4a90dbfe8b4e66ad108
lumberman <vlad@mitkovsky.com>
2021-01-20 23:37:18 -0400

    New Testing Enviroment

---
+++ __mock__/i18next.ts
@@ -0,0 +1,55 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+	.use(initReactI18next)
+	.init({
+		lng: 'en',
+		fallbackLng: ['en', 'es', 'fr', 'pt'],
+
+		// have a common namespace used around the full app
+		ns: ['common'],
+		defaultNS: 'common',
+		fallbackNS: 'common',
+
+		debug: false,
+
+		interpolation: {
+			escapeValue: false, // not needed for react!!
+		},
+
+		resources: {
+			es: {
+				welcome: {
+					botMessageTitle: "Bienvenido a Voke!"
+				}
+			},
+			fr: {
+				welcome: {
+					botMessageTitle: "Bienvenue à Voke!"
+				}
+			},
+			'pt-BR': {
+				welcome: {
+					botMessageTitle: "Bem-vindo ao Voke!"
+				}
+			},
+			pt: {
+				welcome: {
+					botMessageTitle: "Bem-vindo ao Voke!"
+				}
+			},
+			'en-US': {
+				welcome: {
+					botMessageTitle: "Welcome to Voke!"
+				}
+			},
+			en: {
+				welcome: {
+					botMessageTitle: "Welcome to Voke!"
+				}
+			}
+		},
+	});
+
+export default i18n;
