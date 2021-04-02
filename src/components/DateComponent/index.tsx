b812bdf505db3e74f6ca79edac80c7bc5bda5008
Vlad Mitkovsky <vlad@mitkovsky.com>
2021-04-01 21:56:48 -0300

    VC-1417: Main question card > Footer > Invalid date
    https://jesusfilmmedia.atlassian.net/browse/VC-1417

--- src/components/DateComponent/index.tsx
+++ src/components/DateComponent/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import moment from 'moment';
 import { isString, momentUtc } from 'utils';
 
 import Text from '../Text';
 
-function DateComponent({ date, format = 'ddd, lll', ...rest }) {
+function DateComponent({
+  date = '',
+  format = 'ddd, lll',
+  ...rest
+}): ReactElement {
   let text;
   if (isString(date) && date.indexOf('UTC') >= 0) {
     text = momentUtc(date).local().format(format);
   } else {
-    text = moment(date).format(format);
+    const messageDate = moment(date);
+    text = messageDate.isValid() ? moment(date).format(format) : '';
   }
   return <Text {...rest}>{text}</Text>;
 }
