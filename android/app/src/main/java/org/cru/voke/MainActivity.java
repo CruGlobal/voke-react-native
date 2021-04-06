6364ad0bd763b8eabc7504de97258da03e7442fc
Vlad Mitkovsky <linkedin@mitkovsky.com>
2021-04-06 15:49:10 -0300

    VC-1418: Leader Zone > Step > Users Modal - Can’t exit out of this modal (#624)
    https://jesusfilmmedia.atlassian.net/browse/VC-1418
    
    Co-authored-by: Vlad Mitkovsky <vlad@mitkovsky.com>

--- android/app/src/main/java/org/cru/voke/MainActivity.java
+++ android/app/src/main/java/org/cru/voke/MainActivity.java
@@ -7,6 +7,7 @@ import com.zoontek.rnbootsplash.RNBootSplash;
 // https://jeremybarbet.github.io/react-native-modalize/#/INSTALLATION
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 // Voke: RN Orientation Locker
 // https://github.com/wonday/react-native-orientation-locker#android
 import android.content.Intent;
@@ -38,5 +39,17 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Voke";
+  }
+
+  // React Native Gesture Handler
+  // https://docs.swmansion.com/react-native-gesture-handler/docs/#updating-mainactivityjava
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
