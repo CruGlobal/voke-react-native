4479c23ad085babc35b888c32d4fdf81f809c0de
Vlad Mitkovsky <vlad@lumbermandesigns.com>
2021-04-20 09:33:52 -0300

    Update src/domain/Adventures/AdvLanguageSwitch/index.tsx
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

--- src/domain/Adventures/AdvLanguageSwitch/index.tsx
+++ src/domain/Adventures/AdvLanguageSwitch/index.tsx
@@ -39,7 +39,7 @@ const AdvLanguageSwitch = (): ReactElement => {
   }, [currentLang]);
 
   const fillLanguages = (): void => {
-    const appLang = i18next?.language ? i18next.language.toUpperCase() : 'EN';
+    const appLang = i18next?.language?.toUpperCase() || 'EN';
     availableTranslations.map(lang => {
       const stepLang = lang.toUpperCase();
       if (appLang === stepLang || (stepLang === 'EN' && appLang === 'EN-US')) {
